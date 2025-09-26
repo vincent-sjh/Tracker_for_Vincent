2108912b8d9964f699daaf995cc708dec2456331
vincent <songjh22@mails.tsinghua.edu.cn>
2025-09-27 02:53:38 +0800

    fix(data): correct duplicated key in fitnessData object

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ const fitnessData = {
     "2025-09-22": { exercise: 0, calorie: 10, discipline: 0 },
     "2025-09-23": { exercise: 10, calorie: 5, discipline: 0 },
     "2025-09-24": { exercise: 10, calorie: 5, discipline: 0 },
-    "2025-09-25": { exercise: 10, calorie: 5, discipline: 0 }
+    "2025-09-25": { exercise: 10, calorie: 5, discipline: 0 },
+    "2025-09-25": { exercise: 0, calorie: 5, discipline: 0 }
 };
 
 class FitnessTracker {
